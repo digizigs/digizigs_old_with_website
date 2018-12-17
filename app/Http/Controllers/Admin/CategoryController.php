a238e675f7f866b71cc52a325814dbba40d7eebb
vishwa <you@example.com>
2018-12-18 00:37:38 +0530

    nothing

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
     
     public function index()
     {
-        return view('admin\posts\category');
+        return view('admin.posts.category');
     }
 
     public function getcategories(){
