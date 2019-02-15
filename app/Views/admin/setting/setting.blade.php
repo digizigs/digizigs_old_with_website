afa8a70f98969635f48230febd1f80a68d3b0319
jaysvishwa <jaysvishwa@gmail.com>
2019-02-15 12:18:52 +0530

    wp-setting stype

--- app/Views/admin/setting/setting.blade.php
+++ app/Views/admin/setting/setting.blade.php
@@ -56,7 +56,7 @@
 
 
 
-                    <div class="setting-page-tab" role="tabpanel" data-example-id="togglable-tabs">
+                    <div class="" role="tabpanel" data-example-id="togglable-tabs">
 	                    <ul id="myTab" class="nav nav-tabs bar_tabs" role="tablist">
 	                        <li role="" class="active">
 	                        	<a href="#tab_content1" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-cog"> </i> General</a>
@@ -71,11 +71,7 @@
 	                        	<a href="#tab_content4" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false">Media</a>
 	                        </li>                      
 	                    </ul>
-                      	<div id="myTabContent" class="tab-content">
-
-
-
-
+                      	<div id="myTabContent" class="tab-content setting-page-tabx">
 
 	                        <div role="tabpanel" class="tab-pane fade active in" id="tab_content1">
 	                         	
@@ -83,7 +79,7 @@
 									<h1>General Settings</h1>
 									<form method="post" action="options.php" >
 										
-										<table class="form-table">
+										<table class="form-table wp-input-box">
 											<tbody>
 												<tr>
 													<th scope="row"><label for="blogname">Site Title</label></th>
@@ -122,44 +118,7 @@
 													</p>
 													</td>
 												</tr>
-												<tr>
-													<th scope="row">Membership</th>
-													<td> 
-														<fieldset>
-															<legend class="screen-reader-text">
-																<span>Membership</span>
-															</legend>
-															<label for="users_can_register">
-																<input name="users_can_register" type="checkbox" id="users_can_register" value="1">
-																	Anyone can register
-															</label>
-														</fieldset>
-													</td>
-												</tr>
-												<tr>
-												<th scope="row"><label for="default_role">New User Default Role</label></th>
-												<td>
-													<select name="default_role" id="default_role">
-														<option selected="selected" value="subscriber">Subscriber</option>
-														<option value="contributor">Contributor</option>
-														<option value="author">Author</option>
-														<option value="editor">Editor</option>
-														<option value="administrator">Administrator</option>
-													</select>
-													</td>
-												</tr>
-
-												<tr>
-												<th scope="row"><label for="start_of_week">Week Starts On</label></th>
-												<td><select name="start_of_week" id="start_of_week">
-													<option value="0">Sunday</option>
-													<option value="1" selected="selected">Monday</option>
-													<option value="2">Tuesday</option>
-													<option value="3">Wednesday</option>
-													<option value="4">Thursday</option>
-													<option value="5">Friday</option>
-													<option value="6">Saturday</option></select></td>
-												</tr>
+																																
 											</tbody>
 										</table>
 										<p class="submit">											
@@ -174,8 +133,9 @@
 								<div class="wrap">
 									<h1>Reading Settings</h1>
 
-									<form method="post" action="options.php">										
-										<table class="form-table">
+									<form method="post" action="options.php">
+
+										<table class="form-table wp-input-box">
 											<tbody>
 												<tr>
 													<th scope="row">Your homepage displays</th>
@@ -223,29 +183,7 @@
 														<input name="posts_per_page" type="number" step="1" min="1" id="posts_per_page" value="10" class="small-text"> posts
 													</td>
 												</tr>
-												<tr>
-													<th scope="row"><label for="posts_per_rss">Syndication feeds show the most recent</label></th>
-													<td>
-														<input name="posts_per_rss" type="number" step="1" min="1" id="posts_per_rss" value="10" class="small-text"> items
-													</td>
-												</tr>
-												<tr>
-													<th scope="row">For each article in a feed, show </th>
-													<td>
-														<fieldset>
-															<legend class="screen-reader-text">
-																<span>For each article in a feed, show </span>
-															</legend>
-															<p>
-																<label>
-																	<input name="rss_use_excerpt" type="radio" value="0" checked="checked"> Full text
-																</label><br>
-																<label>
-																	<input name="rss_use_excerpt" type="radio" value="1"> Summary</label>
-																</p>
-														</fieldset>
-													</td>
-												</tr>
+												
 												
 											</tbody>
 										</table>
@@ -287,96 +225,102 @@
 													</select>
 												</td>
 											</tr>
-										</tbody></table>
-										<h2 class="title">Post via email</h2>
-										<p>To post to WordPress by email you must set up a secret email account with POP3 access. Any mail received at this address will be posted, so it’s a good idea to keep this address very secret. Here are three random strings you could use: <kbd>xbAJLv0L</kbd>, <kbd>2TPVsGvT</kbd>, <kbd>d1PqebsO</kbd>.</p>
-										<table class="form-table">
-											<tbody><tr>
-												<th scope="row"><label for="mailserver_url">Mail Server</label></th>
-												<td><input name="mailserver_url" type="text" id="mailserver_url" value="mail.example.com" class="regular-text code">
-												<label for="mailserver_port">Port</label>
-												<input name="mailserver_port" type="text" id="mailserver_port" value="110" class="small-text">
-											</td>
-										</tr>
-										<tr>
-											<th scope="row"><label for="mailserver_login">Login Name</label></th>
-											<td><input name="mailserver_login" type="text" id="mailserver_login" value="login@example.com" class="regular-text ltr"></td>
-										</tr>
-										<tr>
-											<th scope="row"><label for="mailserver_pass">Password</label></th>
-											<td>
-												<input name="mailserver_pass" type="text" id="mailserver_pass" value="password" class="regular-text ltr">
-											</td>
-										</tr>
-										<tr>
-											<th scope="row"><label for="default_email_category">Default Mail Category</label></th>
-											<td>
-												<select name="default_email_category" id="default_email_category" class="postform">
-													<option class="level-0" value="1" selected="selected">Uncategorized</option>
-												</select>
-											</td>
-										</tr>
-									</tbody></table>
-									<h2 class="title">Update Services</h2>
-									<p><label for="ping_sites">When you publish a new post, WordPress automatically notifies the following site update services. For more about this, see <a href="https://codex.wordpress.org/Update_Services">Update Services</a> on the Codex. Separate multiple service URLs with line breaks.</label></p>
-									<textarea name="ping_sites" id="ping_sites" class="large-text code" rows="3">http://rpc.pingomatic.com/</textarea>
-									<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p></form>
+										</tbody>
+									</table>
+																			
+									
+									<p class="submit">
+										<button class="btn btn-dark btn-sm">Save</button>
+									</p>
+								</form>
 								</div>
 	                        </div>
 
 	                        <div role="tabpanel" class="tab-pane fade" id="tab_content4">
 								<div class="wrap">
 									<h1>Media Settings</h1>
+
 									<form action="options.php" method="post">
-										<input type="hidden" name="option_page" value="media"><input type="hidden" name="action" value="update"><input type="hidden" id="_wpnonce" name="_wpnonce" value="91db30165f"><input type="hidden" name="_wp_http_referer" value="/wordpress/wp-admin/options-media.php">
+										
 										<h2 class="title">Image sizes</h2>
-										<p>The sizes listed below determine the maximum dimensions in pixels to use when adding an image to the Media Library.</p>
+										<p>
+											The sizes listed below determine the maximum dimensions in pixels to use when adding an image to the Media Library.
+										</p>
+										<table class="form-table wp-input-box">
+											<tbody>
+												<tr>
+													<th scope="row">Thumbnail size</th>
+													<td>
+														<fieldset>
+															<legend class="screen-reader-text">
+																<span>Thumbnail size</span>
+															</legend>
+															<label for="thumbnail_size_w">Width</label>
+															<input name="thumbnail_size_w" type="number" step="1" min="0" id="thumbnail_size_w" value="150" class="small-text">
+
+															<br>
+
+															<label for="thumbnail_size_h">Height</label>
+															<input name="thumbnail_size_h" type="number" step="1" min="0" id="thumbnail_size_h" value="150" class="small-text">
+														</fieldset>
+															<input name="thumbnail_crop" type="checkbox" id="thumbnail_crop" value="1" checked="checked">
+															<label for="thumbnail_crop">
+																Crop thumbnail to exact dimensions (normally thumbnails are proportional)
+															</label>
+													</td>
+												</tr>
+												<tr>
+													<th scope="row">Medium size</th>
+													<td>
+														<fieldset>
+															<legend class="screen-reader-text">
+																<span>Medium size</span>
+															</legend>
+															<label for="medium_size_w">Max Width</label>
+															<input name="medium_size_w" type="number" step="1" min="0" id="medium_size_w" value="300" class="small-text">
+
+															<br>
+
+															<label for="medium_size_h">Max Height</label>
+															<input name="medium_size_h" type="number" step="1" min="0" id="medium_size_h" value="300" class="small-text">
+														</fieldset>
+													</td>
+												</tr>
+												<tr>
+													<th scope="row">Large size</th>
+													<td>
+														<fieldset>
+															<legend class="screen-reader-text">
+																<span>Large size</span>
+															</legend>
+															<label for="large_size_w">Max Width</label>
+															<input name="large_size_w" type="number" step="1" min="0" id="large_size_w" value="1024" class="small-text">
+
+															<br>
+
+															<label for="large_size_h">Max Height</label>
+															<input name="large_size_h" type="number" step="1" min="0" id="large_size_h" value="1024" class="small-text">
+														</fieldset>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+
+										<h2 class="title">Uploading Files</h2>
 										<table class="form-table">
-											<tbody><tr>
-												<th scope="row">Thumbnail size</th>
-												<td><fieldset><legend class="screen-reader-text"><span>Thumbnail size</span></legend>
-												<label for="thumbnail_size_w">Width</label>
-												<input name="thumbnail_size_w" type="number" step="1" min="0" id="thumbnail_size_w" value="150" class="small-text">
-												<br>
-												<label for="thumbnail_size_h">Height</label>
-												<input name="thumbnail_size_h" type="number" step="1" min="0" id="thumbnail_size_h" value="150" class="small-text">
-											</fieldset>
-											<input name="thumbnail_crop" type="checkbox" id="thumbnail_crop" value="1" checked="checked">
-											<label for="thumbnail_crop">Crop thumbnail to exact dimensions (normally thumbnails are proportional)</label>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">Medium size</th>
-										<td><fieldset><legend class="screen-reader-text"><span>Medium size</span></legend>
-										<label for="medium_size_w">Max Width</label>
-										<input name="medium_size_w" type="number" step="1" min="0" id="medium_size_w" value="300" class="small-text">
-										<br>
-										<label for="medium_size_h">Max Height</label>
-										<input name="medium_size_h" type="number" step="1" min="0" id="medium_size_h" value="300" class="small-text">
-									</fieldset></td>
-								</tr>
-								<tr>
-									<th scope="row">Large size</th>
-									<td><fieldset><legend class="screen-reader-text"><span>Large size</span></legend>
-									<label for="large_size_w">Max Width</label>
-									<input name="large_size_w" type="number" step="1" min="0" id="large_size_w" value="1024" class="small-text">
-									<br>
-									<label for="large_size_h">Max Height</label>
-									<input name="large_size_h" type="number" step="1" min="0" id="large_size_h" value="1024" class="small-text">
-								</fieldset></td>
-								</tr>
-								</tbody></table>
-								<h2 class="title">Uploading Files</h2>
-								<table class="form-table">
-								<tbody><tr>
-								<td class="td-full">
-									<label for="uploads_use_yearmonth_folders">
-										<input name="uploads_use_yearmonth_folders" type="checkbox" id="uploads_use_yearmonth_folders" value="1" checked="checked">
-									Organize my uploads into month- and year-based folders</label>
-								</td>
-								</tr>
-								</tbody></table>
-								<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>
+											<tbody>
+												<tr>
+													<td class="td-full">
+														<label for="uploads_use_yearmonth_folders">
+														<input name="uploads_use_yearmonth_folders" type="checkbox" id="uploads_use_yearmonth_folders" value="1" checked="checked">
+															Organize my uploads into month- and year-based folders</label>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+										<p class="submit">
+											<button class="btn btn-dark btn-sm">Save</button>
+										</p>
 								</form>
 								</div>
 	                        </div>
