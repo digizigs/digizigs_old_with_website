704b9ab2edb46de1e3661dc72dbceb55f0aa6fac
root <root@ip-172-31-28-53.ap-south-1.compute.internal>
2019-07-12 13:24:01 +0000

    local

--- app/Http/Controllers/Admin/Mailbox/MgInboundController.php
+++ app/Http/Controllers/Admin/Mailbox/MgInboundController.php
@@ -9,29 +9,18 @@ use Log;
 
 class MgInboundController extends Controller
 {
-<<<<<<< HEAD:app/Http/Controllers/MailgunWidgetsController.php
-    public function store(Request $request)
-=======
+	public function store(Request $request){
 
-    public function store()
->>>>>>> 940ed96230cbf60d1a3abd2f697305e201119354:app/Http/Controllers/Admin/Mailbox/MgInboundController.php
-    {
-        app('log')->debug(request()->all());
-	//Log::info($request->all());
-	$subject = $request->subject;
-        $test = new Test;
-<<<<<<< HEAD:app/Http/Controllers/MailgunWidgetsController.php
-   	$test->type = 'Mailgun';
-   	$test->value = $subject;
-   	$test->save();
-=======
-   		$test->type = 'Mailgun';
-   		$test->value = 'test';
+	        app('log')->debug(request()->all());
+
+        	$test = new Test;
+   		$test->type = 'Sender';
+   		$test->value = $request->sender;
    		$test->save();
->>>>>>> 940ed96230cbf60d1a3abd2f697305e201119354:app/Http/Controllers/Admin/Mailbox/MgInboundController.php
 
 
         return response()->json(['status' => 'ok']);
 
-    }
+    	}
+
 }
