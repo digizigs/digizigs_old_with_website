fedef2d8c0fcbcc1784dcb372870509edf92b9dc
root <root@ip-172-31-28-53.ap-south-1.compute.internal>
2019-07-12 13:11:40 +0000

    local

--- app/Http/Controllers/MailgunWidgetsController.php
+++ app/Http/Controllers/MailgunWidgetsController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Log;
 
 class MailgunWidgetsController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
         app('log')->debug(request()->all());
-
+	//Log::info($request->all());
+	$subject = $request->subject;
         $test = new Test;
-   		$test->type = 'Mailgun';
-   		$test->value = $request->From;
-   		$test->save();
+   	$test->type = 'Mailgun';
+   	$test->value = $subject;
+   	$test->save();
 
 
         return response()->json(['status' => 'ok']);
