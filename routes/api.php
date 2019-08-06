ef8af0bf15de7214cbed6b924f5d69e649a526ab
jaysvishwa <jaysvishwa@gmail.com>
2019-08-06 16:30:22 +0530

    local

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::group(['prefix' => 'mailgun'],function () {
     
 });
 
-Route::apiResource('mails', 'Api\Mailcontroller');
+Route::apiResource('mails', 'Api\MailController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
