1c978403583a66d6ff90d099b9fa76c0b10743e0
jaysvishwa <jaysvishwa@gmail.com>
2019-08-19 10:30:20 +0530

    local

---
+++ app/Views/admin/pages/oauth/oauth.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.admin')
+
+@section('title','OAuth')
+
+
+
+@section('content')
+
+<div class="right_col" role="main">
+
+	<clients></clients>
+
+	<authorizedclients></authorizedclients>
+
+	<personalaccesstokens></personalaccesstokens>
+
+</div>
+
+
+@endsection
