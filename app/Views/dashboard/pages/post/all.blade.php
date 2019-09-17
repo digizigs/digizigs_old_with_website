760af587c64803516186983280581adbd4df8976
jaysvishwagit <jaysvishwa@gmail.com>
2019-09-18 00:05:34 +0530

    local

--- app/Views/dashboard/pages/post/all.blade.php
+++ app/Views/dashboard/pages/post/all.blade.php
@@ -6,31 +6,25 @@
 @section('css')
     <link rel="stylesheet" href="{{asset('public/dashboard/assets/css/dashforge.filemgr.css')}}">
 
-    <style>
-      .filemgr-sidebar-bodyss {
-        position: absolute;
-        top: 0px;
-        left: 0;
-        right: 0;
-        bottom: 0; 
-    }
-
-    .filemgr-content-bodyss{
-      position: absolute;
-      top: 0px;
-      left: 0;
-      right: 0;
-      bottom: 0; 
-    }
-
-  .filemgr-content-bodys .mediassss {
-    padding: 15px 20px;
-    background-color: #f8f9fc;
-    transition: all 0.2s ease-in-out; }
-
-
+    <style> 
+      .filemgr-sidebar-body{
+          .nav-aside{
+            ul{
+              li{
+                svg{color: red !important;}
+                a{
+                 span{
+                   color: red !important;
+                 }
+                }
+              }
+            }
+          }
+        }
+        .filemgr-sidebar-body .active svg{
+          color: red !important;
+        }
     </style>
-    
 @endsection
 
 
@@ -47,37 +41,16 @@
     <div class="filemgr-sidebar-body">
         <div class="pd-t-20 pd-b-10 pd-x-20">
             <ul class="nav nav-aside">
-            <li class="nav-label">Dashboard</li>
-            <li class="nav-item active"><a href="dashboard-one.html" class="nav-link"><i data-feather="shopping-bag"></i> <span>Sales Monitoring</span></a></li>
-            <li class="nav-item"><a href="dashboard-two.html" class="nav-link"><i data-feather="globe"></i> <span>Website Analytics</span></a></li>
-            <li class="nav-item"><a href="dashboard-three.html" class="nav-link"><i data-feather="pie-chart"></i> <span>Cryptocurrency</span></a></li>
-            <li class="nav-item"><a href="dashboard-four.html" class="nav-link"><i data-feather="life-buoy"></i> <span>Helpdesk Management</span></a></li>
-            <li class="nav-label mg-t-25">Web Apps</li>
-            <li class="nav-item"><a href="app-calendar.html" class="nav-link"><i data-feather="calendar"></i> <span>Calendar</span></a></li>
-            <li class="nav-item"><a href="app-chat.html" class="nav-link"><i data-feather="message-square"></i> <span>Chat</span></a></li>
-            <li class="nav-item"><a href="app-contacts.html" class="nav-link"><i data-feather="users"></i> <span>Contacts</span></a></li>
-            <li class="nav-item"><a href="app-file-manager.html" class="nav-link"><i data-feather="file-text"></i> <span>File Manager</span></a></li>
-            <li class="nav-item"><a href="app-mail.html" class="nav-link"><i data-feather="mail"></i> <span>Mail</span></a></li>
 
-            <li class="nav-label mg-t-25">Pages</li>
-            <li class="nav-item with-sub">
-                <a href="" class="nav-link"><i data-feather="user"></i> <span>User Pages</span></a>
-                <ul>
-                <li><a href="page-profile-view.html">View Profile</a></li>
-                <li><a href="page-connections.html">Connections</a></li>
-                <li><a href="page-groups.html">Groups</a></li>
-                <li><a href="page-events.html">Events</a></li>
-                </ul>
-            </li>
-            <li class="nav-item with-sub">
-                <a href="" class="nav-link"><i data-feather="file"></i> <span>Other Pages</span></a>
-                <ul>
-                <li><a href="page-timeline.html">Timeline</a></li>
-                </ul>
-            </li>
-            <li class="nav-label mg-t-25">User Interface</li>
-            <li class="nav-item"><a href="../../components" class="nav-link"><i data-feather="layers"></i> <span>Components</span></a></li>
-            <li class="nav-item"><a href="../../collections" class="nav-link"><i data-feather="box"></i> <span>Collections</span></a></li>
+              <li class="nav-label active">Posts</li>
+              <li class="nav-item"><a href="dashboard-two.html" class="nav-link active "><i data-feather="cast"></i> <span>All Post</span></a></li>
+              <li class="nav-item"><a href="dashboard-three.html" class="nav-link"><i data-feather="plus-circle"></i> <span>Add Post</span></a></li>
+
+              <li class="nav-label mg-t-25">Post Types</li>
+              <li class="nav-item"><a href="app-calendar.html" class="nav-link"><i data-feather="maximize"></i> <span>Published</span></a></li>
+              <li class="nav-item"><a href="app-chat.html" class="nav-link"><i data-feather="minimize"></i> <span>Draft</span></a></li>
+              <li class="nav-item"><a href="app-contacts.html" class="nav-link"><i data-feather="trash"></i> <span>Trash</span></a></li>
+
             </ul>
         </div>
     </div>
